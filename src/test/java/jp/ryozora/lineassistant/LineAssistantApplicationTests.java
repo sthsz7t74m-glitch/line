2e8498f5cfbb72b6ce184a918f05c9df65d58714
sthsz7t74m-glitch <304956452+sthsz7t74m-glitch@users.noreply.github.com>
2026-07-23 18:31:40 +0900

    テスト環境ではFlywayを無効化してビルド失敗を防止

--- src/test/java/jp/ryozora/lineassistant/LineAssistantApplicationTests.java
+++ src/test/java/jp/ryozora/lineassistant/LineAssistantApplicationTests.java
@@ -5,7 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
         "line.bot.channel-secret=test",
-        "line.bot.channel-access-token=test"
+        "line.bot.channel-access-token=test",
+        "spring.datasource.url=jdbc:h2:mem:lineassistant;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.flyway.enabled=false",
+        "spring.task.scheduling.enabled=false"
 })
 class LineAssistantApplicationTests {
     @Test
